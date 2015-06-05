6867ad986ff7ff45faf90832c33d458ef1b7cce3
SlayerBirden <olegkulik1985@gmail.com>
2015-06-05 14:30:03 +0300

    rework diff copy

--- src/MagentoHackathon/Composer/Magento/Deploystrategy/Diff.php
+++ src/MagentoHackathon/Composer/Magento/Deploystrategy/Diff.php
@@ -2,7 +2,7 @@
 
 namespace MagentoHackathon\Composer\Magento\Deploystrategy;
 
-class Diff extends Copy
+class Diff extends DeploystrategyAbstract
 {
     /**
      * {@inheritdoc}
@@ -28,10 +28,23 @@ class Diff extends Copy
 
     /**
      * {@inheritdoc}
-     * don't add deployed files
      */
-    public function addDeployedFile($file)
+    public function createDelegate($source, $dest)
     {
-        return;#pass
+        $sourcePath = $this->getSourceDir() . '/' . $this->removeTrailingSlash($source);
+        $destPath = $this->getDestDir() . '/' . $this->removeTrailingSlash($dest);
+        if (!is_file($sourcePath)) {
+            throw new \ErrorException("Diff strategy can only accept files.");
+        }
+
+        // Create all directories up to one below the target if they don't exist
+        $destDir = dirname($destPath);
+        if (!file_exists($destDir)) {
+            mkdir($destDir, 0777, true);
+        }
+        if (file_exists($destPath)) {
+            throw new \ErrorException(sprintf("Target %s already exists. Can't use force for diff strategy.", $dest));
+        }
+        return copy($sourcePath, $destPath);
     }
 }
